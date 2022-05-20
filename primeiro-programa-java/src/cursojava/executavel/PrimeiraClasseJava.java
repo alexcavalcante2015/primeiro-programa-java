64c1f32b703673178c511b00d3eb30fd2ebce0af
Alex <Alex@DESKTOP-5DKHRF1>
2022-05-20 07:26:12 -0300

    Fim AULA 9.17 - Criando atributos de disciplina do aluno

--- primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
+++ primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -14,7 +14,7 @@ public class PrimeiraClasseJava {
 		 * aluno1 é uma referência para o objeto aluno
 		 */
 		
-		/*String nome = JOptionPane.showInputDialog("Qual o nome do aluno?");
+		String nome = JOptionPane.showInputDialog("Qual o nome do aluno?");
 		String idade = JOptionPane.showInputDialog("Qual a idade?");
 		String dataNascimento = JOptionPane.showInputDialog("Data de nascimento?");
 		String rg = JOptionPane.showInputDialog("Registro Geral?");
@@ -24,9 +24,17 @@ public class PrimeiraClasseJava {
 		String dataMatricula = JOptionPane.showInputDialog("Data da matrícula");
 		String serie = JOptionPane.showInputDialog("Qual a serie");
 		String escola = JOptionPane.showInputDialog("Nome da escola");
+		
+		String disciplina1 = JOptionPane.showInputDialog("Disciplina 1?");
 		String nota1 = JOptionPane.showInputDialog("Nota 1");
+		
+		String disciplina2 = JOptionPane.showInputDialog("Disciplina 2?");
 		String nota2 = JOptionPane.showInputDialog("Nota 2");
+		
+		String disciplina3 = JOptionPane.showInputDialog("Disciplina 3?");
 		String nota3 = JOptionPane.showInputDialog("Nota 3");
+		
+		String disciplina4 = JOptionPane.showInputDialog("Disciplina 4?");
 		String nota4 = JOptionPane.showInputDialog("Nota 4");
 		
 		Aluno aluno1 = new Aluno();
@@ -41,30 +49,22 @@ public class PrimeiraClasseJava {
 		aluno1.setDataMatricula(dataMatricula);
 		aluno1.setSerieMatriculado(serie);
 		aluno1.setNomeEscola(escola);
+		
 		aluno1.setNota1(Double.parseDouble(nota1));
 		aluno1.setNota2(Double.parseDouble(nota2));
 		aluno1.setNota3(Double.parseDouble(nota3));
 		aluno1.setNota4(Double.parseDouble(nota4));
 		
+		aluno1.setDisciplina1(disciplina1);
+		aluno1.setDisciplina2(disciplina2);
+		aluno1.setDisciplina3(disciplina3);
+		aluno1.setDisciplina4(disciplina4);
+		
 		System.out.println(aluno1);// é o mesmo que Sysout(aluno1.toString())
 		System.out.println("Média do aluno = " + aluno1.getMediaNota());
-		System.out.println("Resultado = " + aluno1.getAlunoAprovado2());*/
+		System.out.println("Resultado = " + aluno1.getAlunoAprovado2());
 		
-		// Equals e Hascode (Diferenciar e comparar objetos)
 		
-		Aluno aluno1 = new Aluno();
-		aluno1.setNome("Alex");
-		aluno1.setNumeroCpf("123");
-		
-		Aluno aluno2 = new Aluno();
-		aluno2.setNome("Alex");
-		aluno2.setNumeroCpf("1234");
-		
-		if (aluno1.equals(aluno2)) {
-			System.out.println("Alunos são iguais");
-		} else {
-			System.out.println("Alunos não são iguais");
-		}
 		
 	} 
 	
