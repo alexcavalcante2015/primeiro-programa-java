108c5da21f3eaf410d461aeb0b874be388945d69
Alex <Alex@DESKTOP-5DKHRF1>
2022-05-23 08:27:30 -0300

    Fim AULA 9.28 - Procurando e removendo um aluno da lista

--- primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
+++ primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -86,13 +86,24 @@ public class PrimeiraClasseJava {
 		for (Aluno aluno : alunos) {
 			
 			if (aluno.getNome().equalsIgnoreCase("alex")) {
-				
+				alunos.remove(aluno);
+				break;
+			} else {
 				System.out.println(aluno);// é o mesmo que Sysout(aluno1.toString())
 				System.out.println("Média do aluno = " + aluno.getMediaNota());
 				System.out.println("Resultado = " + aluno.getAlunoAprovado2());
 				System.out.println("----------------------------------------------------------");
-				break;
-				
+			}
+			
+		}
+		
+		for (Aluno aluno : alunos) {
+			System.out.println("Alunos que sobraram na lista");
+			System.out.println(aluno.getNome());
+			System.out.println("Suas matérias são");
+			
+			for (Disciplina disciplina : aluno.getDisciplinas()) {
+				System.out.println(disciplina.getDisciplina());
 			}
 			
 		}
