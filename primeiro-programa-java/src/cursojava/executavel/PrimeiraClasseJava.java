469bcb9d6ae5f6839d4c660af9d81cfc01dec94c
Alex <Alex@DESKTOP-5DKHRF1>
2022-05-23 08:43:30 -0300

    Fim AULA 9.29 - Percorrendo lista pelas posições

--- primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
+++ primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -15,7 +15,7 @@ public class PrimeiraClasseJava {
 		
 		List<Aluno> alunos = new ArrayList<Aluno>();
 		
-		for (int qtd = 1; qtd <= 2; qtd++) {
+		for (int qtd = 1; qtd <= 1; qtd++) {
 		
 		/* 
 		 * new Aluno() é uma instância (Criação de Objeto)
@@ -83,27 +83,20 @@ public class PrimeiraClasseJava {
 		
 		}
 		
-		for (Aluno aluno : alunos) {
+		for (int pos = 0; pos < alunos.size(); pos++) {
 			
-			if (aluno.getNome().equalsIgnoreCase("alex")) {
-				alunos.remove(aluno);
-				break;
-			} else {
-				System.out.println(aluno);// é o mesmo que Sysout(aluno1.toString())
-				System.out.println("Média do aluno = " + aluno.getMediaNota());
-				System.out.println("Resultado = " + aluno.getAlunoAprovado2());
-				System.out.println("----------------------------------------------------------");
-			}
+			Aluno aluno = alunos.get(pos);
 			
-		}
-		
-		for (Aluno aluno : alunos) {
-			System.out.println("Alunos que sobraram na lista");
-			System.out.println(aluno.getNome());
-			System.out.println("Suas matérias são");
+			System.out.println("Aluno = " + aluno.getNome());
+			System.out.println("Média do aluno = " + aluno.getMediaNota());
+			System.out.println("Resultado = " + aluno.getAlunoAprovado2());
+			System.out.println("-------------------------------------------------------------");
 			
-			for (Disciplina disciplina : aluno.getDisciplinas()) {
-				System.out.println(disciplina.getDisciplina());
+			for (int posd = 0; posd < aluno.getDisciplinas().size(); posd++) {
+				
+				Disciplina disc = aluno.getDisciplinas().get(posd);
+				System.out.println("Matéria = " + disc.getDisciplina() + " Nota = " + disc.getNota());
+			
 			}
 			
 		}
