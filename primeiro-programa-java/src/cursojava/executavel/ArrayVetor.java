b7ae81d2dba0ac3995fa8189a2c45ed6f160f7f5
Alex <Alex@DESKTOP-5DKHRF1>
2022-06-19 07:38:53 -0300

    Fim AULA 13.5 - Diferentes formas de atribuição de valores

--- primeiro-programa-java/src/cursojava/executavel/ArrayVetor.java
+++ primeiro-programa-java/src/cursojava/executavel/ArrayVetor.java
@@ -1,27 +1,13 @@
 package cursojava.executavel;
 
-import javax.swing.JOptionPane;
-
 public class ArrayVetor {
 
 	/* Usado para executar código */
 	public static void main(String[] args) {
 		
-		/* Array pode ser de todos os tipos de dados e objetos também */
+		int[] inteiros = new int[10];
+		float[] floats = new float[10];
 		
-		String posicoes = JOptionPane.showInputDialog("Quantas posições o Array deve ter?");
-		
-		/* Array sempre deve ter a quantidade de posições definidas */
-		double[] notas =  new double[Integer.parseInt(posicoes)];
-		
-		for (int pos = 0; pos < notas.length; pos++) {
-			String valor = JOptionPane.showInputDialog("Qual o valor da posição = " + pos);
-			notas[pos] = Double.valueOf(valor);
-		}
-		
-		for (int pos = 0; pos < notas.length; pos++) {
-			System.out.println("Nota " + (pos + 1) + " é = " + notas[pos]);
-		}
 	}
 
 }
