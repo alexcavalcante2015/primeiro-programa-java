9efefa8df952b40c7b981fb4eca9f775bc99fbf6
Alex <Alex@DESKTOP-5DKHRF1>
2022-06-20 07:23:17 -0300

    Fim AULA 13.6 - Adicionando o array de notas dentro da disciplina

--- primeiro-programa-java/src/cursojava/executavel/ArrayVetor.java
+++ primeiro-programa-java/src/cursojava/executavel/ArrayVetor.java
@@ -1,12 +1,33 @@
 package cursojava.executavel;
 
+import cursojava.classes.Aluno;
+import cursojava.classes.Disciplina;
+
 public class ArrayVetor {
 
 	/* Usado para executar código */
 	public static void main(String[] args) {
 		
-		int[] inteiros = new int[10];
-		float[] floats = new float[10];
+		double[] notas = {8.8, 9.7, 7.6, 6.8};
+		double[] notasLogica = {7.1, 5.7, 9.6, 7.8};
+		
+		/*Criação do aluno*/
+		Aluno aluno = new Aluno();
+		aluno.setNome("Alex Fernando Egidio");
+		aluno.setNomeEscola("JDEV Treinamento");
+		
+		//Criação da disciplina
+		Disciplina disciplina = new Disciplina();
+		disciplina.setDisciplina("Curso de Java");
+		disciplina.setNota(notas);
+		
+		aluno.getDisciplinas().add(disciplina);
+		
+		Disciplina disciplina2 = new Disciplina();
+		disciplina2.setDisciplina("Lógica de programação");
+		disciplina2.setNota(notasLogica);
+		
+		aluno.getDisciplinas().add(disciplina2);
 		
 	}
 
