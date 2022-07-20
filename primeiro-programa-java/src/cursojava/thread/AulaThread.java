40a1920a1042144c4902149178b979a078247813
Alex <Alex@DESKTOP-5DKHRF1>
2022-07-20 08:16:09 -0300

    Fim AULA 14.4 - Processamento concorrente entre duas Thread

--- primeiro-programa-java/src/cursojava/thread/AulaThread.java
+++ primeiro-programa-java/src/cursojava/thread/AulaThread.java
@@ -6,7 +6,7 @@ public class AulaThread {
 
 	public static void main(String[] args) throws InterruptedException {
 
-		/*Thread processando em paralelo*/
+		/*Thread processando em paralelo do envio de email*/
 		new Thread() {
 			
 			public void run() {/*Executa o que nós queremos*/
@@ -30,6 +30,31 @@ public class AulaThread {
 		}.start();/*Start liga a thread que fica processando paralelamente por trás*/
 		
 		
+		//****************** DIVISÃO DAS THREAD
+		
+		
+		/*Thread processando em paralelo do envio de nota fiscal*/
+		new Thread() {
+			
+			public void run() {/*Executa o que nós queremos*/
+				/*Código da rotina*/
+				/*Código da rotina que eu quero executar em paralelo*/
+				for (int pos = 0; pos < 10; pos++) {
+					
+					/*Quero executar esse envio com um tempo de parada, ou com um tempo determinado*/
+					System.out.println("Executando alguma rotina, por exemplo, envio de nota fiscal");
+					
+					try {
+						Thread.sleep(4000);/*Dá um tempo*/
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+					
+				}
+				/*Fim do código em paralelo*/
+			};
+			
+		}.start();/*Start liga a thread que fica processando paralelamente por trás*/
 		
 		/*Còdigo do sistema do usuário continua o fluxo de trabalho*/
 		System.out.println("CHEGOU AO FIM DO CÓDIGO DE TESTE DE THREAD");
