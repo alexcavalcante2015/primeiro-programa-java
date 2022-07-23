e3a4c7db790e6fbd22df8b7a1ecd79c48fb4070b
Alex <Alex@DESKTOP-5DKHRF1>
2022-07-23 08:24:58 -0300

    Fim AULA 14.9 - Controlando a Thead 1 com Start e Stop

--- primeiro-programa-java/src/cursojava/thread/TelaTimeThread.java
+++ primeiro-programa-java/src/cursojava/thread/TelaTimeThread.java
@@ -5,7 +5,12 @@ import java.awt.Dimension;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
+import javax.sql.rowset.CachedRowSet;
 import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JLabel;
@@ -25,6 +30,25 @@ public class TelaTimeThread extends JDialog {
 	
 	private JButton jButton = new JButton("Start");
 	private JButton jButton2 = new JButton("Stop");
+	
+	private Runnable thread1 = new Runnable() {
+		
+		@Override
+		public void run() { 
+			while (true) { /*Fica sempre rodando*/
+				mostraTempo.setText(new SimpleDateFormat("dd/MM/yyyy hh:mm.ss").
+						format(Calendar.getInstance().getTime()));
+				try {
+					Thread.sleep(1000);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+			
+		}
+	};
+	
+	private Thread thread1Time;
 	
 
 	public TelaTimeThread() {/*Executa o que tiver dentro no momento da abertura ou execução*/
@@ -71,6 +95,25 @@ public class TelaTimeThread extends JDialog {
 		jButton2.setPreferredSize(new Dimension(92, 25));
 		gridBagConstraints.gridx++;
 		jPanel.add(jButton2, gridBagConstraints);
+		
+		jButton.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) { /*Executa o clique no botão*/
+
+				thread1Time = new Thread(thread1);
+				thread1Time.start();
+				
+			}
+		});
+		
+		jButton2.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				thread1Time.stop();
+			}
+		});
 		
 		add(jPanel, BorderLayout.WEST);
 		/*Sempre será o último comando*/
