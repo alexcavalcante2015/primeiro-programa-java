0f7eb4ee4d8fea0c3f8923d0994aa59db63c3dfc
Alex <Alex@DESKTOP-5DKHRF1>
2022-07-28 09:42:54 -0300

    Fim AULA 15.12 - Faixa de tempo com Objeto Period

---
+++ primeiro-programa-java/src/modulo_datas/DatasEmJava8.java
@@ -0,0 +1,34 @@
+package modulo_datas;
+
+import java.time.LocalDate;
+import java.time.Period;
+
+public class DatasEmJava8 {
+
+	public static void main(String[] args) {
+
+		LocalDate dataAntiga = LocalDate.parse("2019-10-05");
+		
+		LocalDate dataNova = LocalDate.parse("2021-10-10");
+		
+		System.out.println("Data antiga é maior que data nova: " + dataAntiga.isAfter(dataNova));
+		
+		System.out.println("Data antiga é anterior a nova: " + dataAntiga.isBefore(dataNova));
+		
+		System.out.println("Datas são iguais: " + dataAntiga.isEqual(dataNova));
+		
+		Period periodo = Period.between(dataAntiga, dataNova);
+		
+		System.out.println("Quantos dias: " + periodo.getDays());
+		
+		System.out.println("Quantos meses: " + periodo.getMonths());
+		
+		System.out.println("Quantos anos: " + periodo.getYears());
+		
+		System.out.println("Somente meses: " + periodo.toTotalMonths());
+		
+		System.out.println("Periodo é : " + periodo.getYears() + " anos, " + periodo.getMonths() + " meses e " + periodo.getDays() + " dias.");
+		
+	}
+
+}
